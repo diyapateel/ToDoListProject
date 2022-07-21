6559115eea6dc2c558604eb744cf1d2dfc53786e
Diya Patel <dipate69@gmail.com>
2022-07-21 10:28:21 -0400

    Initial Commit

---
+++ ToDoListProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListProject
+//
+//  Created by Diya Patel on 7/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
